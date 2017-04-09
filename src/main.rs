161afb1469af3952847813bbef97a85a5b26c6d1
Janis <ueebj@student.kit.edu>
2017-04-09 12:14:08 +0200

    try to setup usb controller

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 #![no_std]
 #![no_main]
+#![feature(asm)]
+#[allow(unused_variables)]
 
 extern crate stm32f7_discovery as stm32f7;
+extern crate embedded_stm32f7;
 
 // initialization routines for .data and .bss
 extern crate r0;
+extern crate cortex_m;
 
-use stm32f7::{sdram, lcd, system_clock, board, embedded};
+use stm32f7::{system_clock, board};
+use embedded_stm32f7::otg_hs_global::{OtgHsGintsts};
 
 #[no_mangle]
 pub unsafe extern "C" fn reset() -> ! {
@@ -29,10 +34,8 @@ pub unsafe extern "C" fn reset() -> ! {
 	r0::init_data(data_start, data_end, data_load);
 	// zeroes the .bss section
 	r0::zero_bss(bss_start, bss_end);
-	unsafe {
-		let scb = stm32f7::cortex_m::peripheral::scb_mut();
-		scb.cpacr.modify(|v| v | 0b1111 << 20);
-	}
+	let scb = stm32f7::cortex_m::peripheral::scb_mut();
+	scb.cpacr.modify(|v| v | 0b1111 << 20);
 
 	main(board::hw());
 }
@@ -59,40 +62,120 @@ fn main(hw: board::Hardware) -> ! {
 		syscfg,
 		ethernet_mac,
 		ethernet_dma,
+		otg_hs_global,
+		otg_hs_device,
+		nvic,
 		.. } = hw;
 
-	use embedded::interfaces::gpio::{self, Gpio};
-	let mut gpio = Gpio::new(gpio_a,
-		gpio_b,
-		gpio_c,
-		gpio_d,
-		gpio_e,
-		gpio_f,
-		gpio_g,
-		gpio_h,
-		gpio_i,
-		gpio_j,
-		gpio_k);
-
 	system_clock::init(rcc, pwr, flash);   
-	// enable all gpio ports
-	rcc.ahb1enr.update(|r| {
-		r.set_gpioaen(true);
-		r.set_gpioben(true);
-		r.set_gpiocen(true);
-		r.set_gpioden(true);
-		r.set_gpioeen(true);
-		r.set_gpiofen(true);
-		r.set_gpiogen(true);
-		r.set_gpiohen(true);
-		r.set_gpioien(true);
-		r.set_gpiojen(true);
-		r.set_gpioken(true);
-	});
+	rcc.ahb1enr.update(|r| r.set_otghsen(true));
+	rcc.ahb1enr.update(|r| r.set_otghsulpien(true));
 
-	sdram::init(rcc, fmc, &mut gpio);
-	let mut lcd = lcd::init(ltdc, rcc, &mut gpio);
+	let ictr: u8 = nvic.ictr.read().intlinesnum();
+	let ictr_addr = &nvic.ictr;
 
+	let iser0: u32 = nvic.iser0.read().setena();
+	let iser1: u32 = nvic.iser1.read().setena();
+	let iser2: u32 = nvic.iser2.read().setena();
+	nvic.iser0.update(|r| r.set_setena(!0));
+	nvic.iser1.update(|r| r.set_setena(!0));
+	nvic.iser2.update(|r| r.set_setena(!0));
+	let iser0_addr = &nvic.iser0;
+	let _iser0: u32 = nvic.iser0.read().setena();
+	let _iser1: u32 = nvic.iser1.read().setena();
+	let _iser2: u32 = nvic.iser2.read().setena();
+	let iabr0 = nvic.iabr0.read().active();
+	let iabr1 = nvic.iabr1.read().active();
+	let iabr2 = nvic.iabr2.read().active();
+	nvic.icpr0.update(|r| r.set_clrpend(!0));
+	nvic.icpr1.update(|r| r.set_clrpend(!0));
+	nvic.icpr2.update(|r| r.set_clrpend(!0));
+	nvic.ipr19.update(|r| r.set_ipr_n1(1)); // set priority of irq77
+	//unsafe { self.iser[usize::from(nr / 32)].write(1 << (nr % 32)) }
+	
+	//fn abc () {
+	//}
+
+	//unsafe { stm32f7::interrupts::HANDLE_INT = Some(abc); }
+	let mut stir : embedded_stm32f7::nvic::Stir = embedded_stm32f7::nvic::Stir::default();
+	stir.set_intid(76);
+	nvic.stir.write(stir);
+
+	// Clear Gintsts to avoid interrupts before init
+	otg_hs_global.otg_hs_gintsts.update(|_| return);
+
+	//core init
+
+	otg_hs_global.otg_hs_gccfg.update(|r| r.set_pwrdwn(false));
+
+	otg_hs_global.otg_hs_gusbcfg.update(|r| r.set_physel(false));
+	otg_hs_global.otg_hs_gusbcfg.update(|r| r.set_tsdps(false));
+	otg_hs_global.otg_hs_gusbcfg.update(|r| r.set_ulpifsls(false));
+
+	otg_hs_global.otg_hs_gusbcfg.update(|r| r.set_ulpievbusd(false));
+	otg_hs_global.otg_hs_gusbcfg.update(|r| r.set_ulpievbusi(false));
+	
+	while ! otg_hs_global.otg_hs_grstctl.read().ahbidl() {};
+	otg_hs_global.otg_hs_grstctl.update(|r| r.set_csrst(true));
+	while otg_hs_global.otg_hs_grstctl.read().csrst() {};
+
+	//options
+
+	otg_hs_global.otg_hs_gahbcfg.update(|r| r.set_gint(true));
+	otg_hs_global.otg_hs_gahbcfg.update(|r| r.set_ptxfelvl(true)); //completely empty
+
+	otg_hs_global.otg_hs_gusbcfg.update(|r| r.set_hnpcap(false));
+	otg_hs_global.otg_hs_gusbcfg.update(|r| r.set_srpcap(false));
+	//otg_hs_global.otg_hs_gusbcfg.update(|r| r.set_tocal()); //not necessary for hs?
+	otg_hs_global.otg_hs_gusbcfg.update(|r| r.set_trdt(0x9)); //only valid value for hs?
+
+	//interrupts
+	otg_hs_global.otg_hs_gintmsk.update(|r| r.set_otgint(true));
+	otg_hs_global.otg_hs_gintmsk.update(|r| r.set_mmism(true));
+
+	// Wait till we enter device mode
+	loop
+	{
+		let cmod = otg_hs_global.otg_hs_gintsts.read().cmod();
+		if !cmod {
+			break;
+		}
+		//sleep
+	}
+
+	otg_hs_global.otg_hs_gccfg.update(|r| r.set_vbden(true));
+	//otg_hs_global.otg_hs_pcgcctl.write(embedded_stm32f7::otg_hs_global::OtgHsPcgcctl::default());
+	//device mode init
+	//options
+	otg_hs_device.otg_hs_dcfg.update(|r| r.set_dspd(0)); // high speed
+	otg_hs_device.otg_hs_dcfg.update(|r| r.set_nzlsohsk(false)); //no clue
+	// interrupts
+	otg_hs_global.otg_hs_gintmsk.update(|r| r.set_esuspm(true));
+	otg_hs_global.otg_hs_gintmsk.update(|r| r.set_usbsuspm(true));
+	otg_hs_global.otg_hs_gintmsk.update(|r| r.set_usbrst(true));
+	otg_hs_global.otg_hs_gintmsk.update(|r| r.set_enumdnem(true));
+	otg_hs_global.otg_hs_gintmsk.update(|r| r.set_sofm(true));
+
+	//let mut full = embedded_stm32f7::otg_hs_global::OtgHsGintmsk::default();
+	//let mask = 0b11111000101111001111110011011110;
+	//full.bits = mask;
+	//otg_hs_global.otg_hs_gintmsk.write(full);
+
+	loop {
+	}
+
+	// Manually poll interrupts (GINTSTS)
+	let mut int : [OtgHsGintsts; 10] = [OtgHsGintsts::default(); 10];
+	let mut i = 0usize;
+	while i < 10usize {
+		let now = otg_hs_global.otg_hs_gintsts.read();
+		otg_hs_global.otg_hs_gintsts.update(|_| return);
+		
+		if now != int[i] {
+			int[i] = now;
+			i += 1;
+		}
+	}
 	loop {
 	}
 }
